297d34d23d8dc81581786653d42f9e3048856df1
arpitat09 <arpita.genai@gmail.com>
2025-08-07 22:08:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TransactionForm from './components/TransactionForm';
+import TransactionList from './components/TransactionList';
 
 function App() {
+  const [refresh, setRefresh] = React.useState(false);
+  const triggerRefresh = () => setRefresh(!refresh);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Finance Budget Tracker</h1>
+      <TransactionForm onAdd={triggerRefresh} />
+      <TransactionList key={refresh} />
     </div>
   );
 }
